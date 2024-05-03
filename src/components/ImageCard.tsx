0a338ff63600158f60f1495c9fcc054ba35070af
prajwalhc-18 <prajwalhc1806@gmail.com>
2024-05-03 22:16:35 +0530

    new code files

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,2 +1,20 @@
+// components/ImageCard.tsx
+
 import React from "react";
-import "./ImageCard.css"
+import "./ImageCard.css"; // Import the CSS file
+
+interface ImageCardProps {
+  imageUrl: string;
+  author: string;
+}
+
+const ImageCard: React.FC<ImageCardProps> = ({ imageUrl, author }) => {
+  return (
+    <div className="image-card">
+      <img src={imageUrl} alt={`Photo by ${author}`} />
+      <div className="author">{author}</div>
+    </div>
+  );
+};
+
+export default ImageCard;
--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,2 +1,37 @@
-import React from "react";
-import ".Pagination.css/"
+// components/Pagination.tsx
+
+import React from 'react';
+
+interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+}
+
+const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPageChange }) => {
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      onPageChange(currentPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      onPageChange(currentPage - 1);
+    }
+  };
+
+  return (
+    <div className="pagination">
+      <button onClick={handlePreviousPage} disabled={currentPage === 1}>
+        Previous
+      </button>
+      <span>{`Page ${currentPage} of ${totalPages}`}</span>
+      <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
