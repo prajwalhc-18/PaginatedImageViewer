3eae86eec49c91b7984f0918b417e6469f93f93e
prajwalhc-18 <prajwalhc1806@gmail.com>
2024-05-04 21:11:46 +0530

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import ImageCard from './components/ImageCard';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import ImageCard from "./components/ImageCard";
+import "./App.css";
 
 const App: React.FC = () => {
   const [images, setImages] = useState<any[]>([]);
@@ -10,16 +10,18 @@ const App: React.FC = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`https://picsum.photos/v2/list?page=${currentPage}&limit=10`);
+        const response = await fetch(
+          `https://picsum.photos/v2/list?page=${currentPage}&limit=10`
+        );
         if (!response.ok) {
-          throw new Error('Failed to fetch images');
+          throw new Error("Failed to fetch images");
         }
         const data = await response.json();
         setImages((prevImages) => [...prevImages, ...data]);
-        const totalPages = Number(response.headers.get('X-Total-Pages'));
+        const totalPages = Number(response.headers.get("X-Total-Pages"));
         setTotalPages(totalPages || 1);
       } catch (error) {
-        console.error('Error fetching images:', error);
+        console.error("Error fetching images:", error);
       }
     };
 
@@ -34,22 +36,29 @@ const App: React.FC = () => {
     <div className="app container">
       <header className="header">
         <div className="logo">
-          <img src="src\assets\gallery-svgrepo-com (2).svg" alt="Logo" className="img-fluid" />
+          <img
+            src="src\assets\gallery-svgrepo-com (2).svg"
+            alt="Logo"
+            className="img-fluid"
+          />
         </div>
         <div className="title">
-          <h1 className="text-center">Paginated Image Viewer</h1>
+          <h1>Paginated Image Viewer</h1>
         </div>
       </header>
-      <div className="image-gallery row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-        {images.map((image) => (
-          <div key={image.id} className="col">
+      <div className="image-gallery grid">
+        {images.map((image, index) => (
+          <div
+            key={image.id}
+            className={`image-card ${index < 5 ? "grid-item" : ""}`}
+          >
             <ImageCard imageUrl={image.download_url} author={image.author} />
           </div>
         ))}
       </div>
       {currentPage < totalPages && (
-        <div className="load-more d-flex justify-content-center">
-          <button className="btn btn-primary" onClick={handleLoadMore}>Load More</button>
+        <div className="load-more">
+          <button onClick={handleLoadMore}>Load More</button>
         </div>
       )}
     </div>
